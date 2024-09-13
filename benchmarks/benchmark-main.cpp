066a01043b6f59ead5077327d6d15d759ab90079
Pieter Pas <pieter.pas@outlook.com>
2024-09-13 15:30:27 +0200

    Improve benchmark OpenMP context

--- benchmarks/benchmark-main.cpp
+++ benchmarks/benchmark-main.cpp
@@ -2,12 +2,16 @@
 #include <koqkatoo-version.h>
 #include <cstdlib>
 
+#include <koqkatoo/openmp.h>
+
 int main(int argc, char **argv) {
     ::benchmark::Initialize(&argc, argv);
     if (::benchmark::ReportUnrecognizedArguments(argc, argv))
         return 1;
-    if (const char *omp_nt = ::getenv("OMP_NUM_THREADS"))
-        benchmark::AddCustomContext("OMP_NUM_THREADS", omp_nt);
+#if KOQKATOO_WITH_OPENMP
+    benchmark::AddCustomContext("OMP_NUM_THREADS",
+                                std::to_string(omp_get_max_threads()));
+#endif
     benchmark::AddCustomContext("koqkatoo_build_time", koqkatoo_build_time);
     benchmark::AddCustomContext("koqkatoo_commit_hash", koqkatoo_commit_hash);
 #if defined(__AVX512F__)
