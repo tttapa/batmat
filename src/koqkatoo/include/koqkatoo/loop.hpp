26e24dc887cb3221e0de0421e42a2c1a1eeb8747
Pieter Pas <pieter.pas@outlook.com>
2024-10-03 14:48:46 +0200

    Add foreach_chunked_merged helper function

--- src/koqkatoo/include/koqkatoo/loop.hpp
+++ src/koqkatoo/include/koqkatoo/loop.hpp
@@ -30,4 +30,24 @@ foreach_chunked(index_t i_begin, index_t i_end, auto chunk_size,
     }
 }
 
+[[gnu::always_inline]] inline void
+foreach_chunked_merged(index_t i_begin, index_t i_end, auto chunk_size,
+                       auto func_chunk, LoopDir dir = LoopDir::Forward) {
+    if (dir == LoopDir::Forward) {
+        index_t i;
+        for (i = i_begin; i + chunk_size <= i_end; i += chunk_size)
+            func_chunk(i, chunk_size);
+        index_t rem_i = i_end - i;
+        if (rem_i > 0)
+            func_chunk(i, rem_i);
+    } else {
+        index_t rem_i = i_end % chunk_size;
+        index_t i     = i_end - rem_i;
+        if (rem_i > 0)
+            func_chunk(i, rem_i);
+        for (i -= chunk_size; i >= i_begin; i -= chunk_size)
+            func_chunk(i, chunk_size);
+    }
+}
+
 } // namespace koqkatoo
