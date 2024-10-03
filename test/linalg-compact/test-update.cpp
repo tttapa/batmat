96d52e25c3d30cba7dd8ea2cf7539e0475d47424
Pieter Pas <pieter.pas@outlook.com>
2024-10-03 14:49:25 +0200

    Block-wise Cholesky downdate Ψ working 🥳

--- test/linalg-compact/test-update.cpp
+++ test/linalg-compact/test-update.cpp
@@ -1,12 +1,16 @@
 #include <koqkatoo/linalg-compact/compact.hpp>
 #include <koqkatoo/linalg-compact/matrix-batch.hpp>
 #include <koqkatoo/linalg-compact/preferred-backend.hpp>
+#include <koqkatoo/loop.hpp>
 #include <Eigen/Core>
 #include <gtest/gtest.h>
 #include <ocp/eigen-matchers.hpp>
 #include <algorithm>
 #include <cmath>
 #include <random>
+#include <vector>
+
+#include <koqkatoo/linalg-compact/compact/micro-kernels/xshh.hpp> // TODO
 
 namespace stdx = std::experimental;
 using namespace koqkatoo;
@@ -34,7 +38,7 @@ TEST(OCP, update) {
     using SBMat [[maybe_unused]] =
         linalg::compact::BatchedMatrix<bool, index_t,
                                        scalar_blas::simd_stride_t>;
-    constexpr index_t N = 3, nx = 2, nu = 1, ny = 3;
+    constexpr index_t N = 5, nx = 7, nu = 1, ny = 11;
     Mat AB{{.depth = N, .rows = nx, .cols = nx + nu}};
     Mat CD{{.depth = N + 1, .rows = ny, .cols = nx + nu}};
     Mat H{{.depth = N + 1, .rows = nx + nu, .cols = nx + nu}};
@@ -148,9 +152,11 @@ TEST(OCP, update) {
     Mat Geᵀ{{.depth = N + 1, .rows = nx + nu, .cols = ny}};
     Mat Σe{{.depth = N + 1, .rows = ny, .cols = 1}};
     Mat Se{{.depth = N + 1, .rows = ny, .cols = 1}};
+    std::vector<index_t> rjs;
     Σe.set_constant(1);
     Se.set_constant(+real_t(0));
     index_t rj_max = 0;
+    rjs.reserve(N + 1);
     for (index_t k = 0; k <= N; ++k) {
         index_t rj = 0;
         for (index_t r = 0; r < ny; ++r) {
@@ -162,6 +168,7 @@ TEST(OCP, update) {
                 ++rj;
             }
         }
+        rjs.push_back(rj);
         rj_max = std::max(rj_max, rj);
     }
     auto Gejᵀ = Geᵀ.view.left_cols(rj_max);
@@ -221,7 +228,9 @@ TEST(OCP, update) {
         W̃_rec(0).block(k * nx + nx, k * ny, nx, ny)      = Ṽe(k);
         W̃_rec(0).block(k * nx + nx, k * ny + ny, nx, ny) = W̃e(k + 1);
     }
+    guanaqo::print_python(std::cout << "W̃ rec:\n", W̃_rec(0));
     scalar_blas::xshh(LΨ̃e_rec, W̃_rec, be);
+    guanaqo::print_python(std::cout << "HH:\n", W̃_rec(0));
 
     guanaqo::print_python(std::cout << "LΨ ref:\n", LΨ̃e_full(0));
     guanaqo::print_python(std::cout << "LΨ rec:\n", LΨ̃e_rec(0));
@@ -232,4 +241,119 @@ TEST(OCP, update) {
     SMat LLΨ̃e_err{{.depth = 1, .rows = (N + 1) * nx, .cols = (N + 1) * nx}};
     scalar_blas::xsub_copy(LLΨ̃e_err, LLΨ̃e_rec, LLΨ̃e_full);
     EXPECT_LE(scalar_blas::xnrminf(LLΨ̃e_err), ε);
+
+    // Block-wise downdate
+    SMat LΨ̃D      = LΨD;
+    SMat LΨ̃S      = LΨS;
+    index_t colsA = 0;
+    SMat A{{.depth = 2, .rows = nx, .cols = (N + 1) * ny}};
+    for (index_t k = 0; k < N; ++k) {
+        using namespace linalg::compact::micro_kernels;
+        static constexpr index_t R = shh::SizeR, S = shh::SizeS;
+        colsA += rjs[k];
+        auto Ad = A.batch(k % 2).left_cols(colsA),
+             As = A.batch((k + 1) % 2).left_cols(colsA);
+        auto Ld = LΨ̃D.batch(k), Ls = LΨ̃S.batch(k);
+        Ad(0).right_cols(rjs[k]) = W̃j(k).left_cols(rjs[k]);
+        As(0).right_cols(rjs[k]) = Ṽj(k).left_cols(rjs[k]);
+
+        guanaqo::print_python(std::cout << "A(" << k << "):\n", Ad(0));
+
+        // Process all diagonal blocks (in multiples of R, except the last).
+        using abi = scalar_abi;
+        foreach_chunked(
+            0, Ld.cols(), R,
+            [&](index_t k) {
+                // Part of A corresponding to this diagonal block
+                auto Add = Ad.middle_rows(k, R);
+                auto Ldd = Ld.block(k, k, R, R);
+                // Process the diagonal block itself
+                using W_t = shh::triangular_accessor<abi, real_t, R>;
+                alignas(W_t::alignment()) real_t W[W_t::size()];
+                shh::xshh_diag_microkernel<abi, R>(colsA, W, Ldd, Add);
+                // Process all rows below the diagonal block (in multiples of S).
+                foreach_chunked_merged(
+                    k + R, Ld.rows(), S, [&](index_t i, auto rem_i) {
+                        auto Ads = Ad.middle_rows(i, rem_i);
+                        auto Lds = Ld.block(i, k, rem_i, R);
+                        shh::microkernel_tail_lut<abi>[rem_i - 1](colsA, W, Lds,
+                                                                  Ads, Add);
+                    }); // TODO: decide on order
+                // Process all rows below the diagonal block (in multiples of S).
+                foreach_chunked_merged(
+                    0, Ls.rows(), S, [&](index_t i, auto rem_i) {
+                        auto Ass = As.middle_rows(i, rem_i);
+                        auto Lss = Ls.block(i, k, rem_i, R);
+                        shh::microkernel_tail_lut<abi>[rem_i - 1](colsA, W, Lss,
+                                                                  Ass, Add);
+                    }); // TODO: decide on order
+            },
+            [&](index_t k, index_t rem_k) {
+                auto Add = Ad.middle_rows(k, rem_k);
+                auto Ldd = Ld.block(k, k, rem_k, rem_k);
+                // Process the diagonal block itself
+                using W_t = shh::triangular_accessor<abi, real_t, R>;
+                alignas(W_t::alignment()) real_t W[W_t::size()];
+                shh::microkernel_diag_lut<abi>[rem_k - 1](colsA, W, Ldd, Add);
+                // Process all rows below the diagonal block (in multiples of S).
+                foreach_chunked_merged(
+                    0, Ls.rows(), S, [&](index_t i, auto rem_i) {
+                        auto Ass = As.middle_rows(i, rem_i);
+                        auto Lss = Ls.block(i, k, rem_i, rem_k);
+                        shh::microkernel_tail_lut_2<abi>[rem_k - 1][rem_i - 1](
+                            colsA, W, Lss, Ass, Add);
+                    }); // TODO: decide on order
+            });
+        guanaqo::print_python(std::cout << "HH(" << k << "):\n", Ad(0));
+        Ad(0).set_constant(0);
+    }
+    {
+        using namespace linalg::compact::micro_kernels;
+        static constexpr index_t R = shh::SizeR, S = shh::SizeS;
+        colsA += rjs[N];
+        auto Ad                  = A.batch(N % 2).left_cols(colsA);
+        auto Ld                  = LΨ̃D.batch(N);
+        Ad(0).right_cols(rjs[N]) = W̃j(N).left_cols(rjs[N]);
+
+        // Process all diagonal blocks (in multiples of R, except the last).
+        using abi = scalar_abi;
+        foreach_chunked(
+            0, Ld.cols(), R,
+            [&](index_t k) {
+                // Part of A corresponding to this diagonal block
+                auto Add = Ad.middle_rows(k, R);
+                auto Ldd = Ld.block(k, k, R, R);
+                // Process the diagonal block itself
+                using W_t = shh::triangular_accessor<abi, real_t, R>;
+                alignas(W_t::alignment()) real_t W[W_t::size()];
+                shh::xshh_diag_microkernel<abi, R>(colsA, W, Ldd, Add);
+                // Process all rows below the diagonal block (in multiples of S).
+                foreach_chunked_merged(
+                    k + R, Ld.rows(), S, [&](index_t i, auto rem_i) {
+                        auto Ads = Ad.middle_rows(i, rem_i);
+                        auto Lds = Ld.block(i, k, rem_i, R);
+                        shh::microkernel_tail_lut<abi>[rem_i - 1](colsA, W, Lds,
+                                                                  Ads, Add);
+                    }); // TODO: decide on order
+            },
+            [&](index_t k, index_t rem_k) {
+                auto Add = Ad.middle_rows(k, rem_k);
+                auto Ldd = Ld.block(k, k, rem_k, rem_k);
+                // Process the diagonal block itself
+                shh::microkernel_full_lut<abi>[rem_k - 1](colsA, Ldd, Add);
+            });
+    }
+
+    // Reconstruct solution
+    SMat LΨ̃e_rec2{{.depth = 1, .rows = (N + 1) * nx, .cols = (N + 1) * nx}};
+    LΨ̃e_rec2(0).top_left(nx, nx) = LΨ̃D(0);
+    for (index_t k = 0; k < N; ++k) {
+        LΨ̃e_rec2(0).block(k * nx + nx, k * nx, nx, nx)      = LΨ̃S(k);
+        LΨ̃e_rec2(0).block(k * nx + nx, k * nx + nx, nx, nx) = LΨ̃D(k + 1);
+    }
+    guanaqo::print_python(std::cout << "LΨ ref:\n", LΨ̃e_full(0));
+    guanaqo::print_python(std::cout << "LΨ rec:\n", LΨ̃e_rec2(0));
+    SMat LΨ̃e_err2{{.depth = 1, .rows = (N + 1) * nx, .cols = (N + 1) * nx}};
+    scalar_blas::xsub_copy(LΨ̃e_err2, LΨ̃e_rec2, LΨ̃e_full);
+    EXPECT_LE(scalar_blas::xnrminf(LΨ̃e_err2), ε);
 }
