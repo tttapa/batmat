8e03e6164b8fb045ed8f9ea3e9c0e44c97b8aa35
Pieter Pas <pieter.p.dev@outlook.com>
2024-10-07 15:54:35 +0200

    Add docstrings UpDown

--- src/cholundate/include/koqkatoo/cholundate/updown.hpp
+++ src/cholundate/include/koqkatoo/cholundate/updown.hpp
@@ -5,11 +5,25 @@
 
 namespace koqkatoo::cholundate {
 
+/// Perform a factorization update, i.e. given the factorization of @f$ K @f$,
+/// compute the factorization of @f$ K + A A^\top @f$.
 struct Update {};
+/// Perform a factorization downdate, i.e. given the factorization of @f$ K @f$,
+/// compute the factorization of @f$ K - A A^\top @f$.
 struct Downdate {};
+/// Perform a factorization update or downdate, depending on the given signs,
+/// i.e. given the factorization of @f$ K @f$, compute the factorization of
+/// @f$ K + A S A^\top @f$, where @f$ S @f$ is a diagonal matrix with
+/// @f$ S_{jj} = 1 @f$ if @p signs[j] is `+0.0`, and @f$ S_{jj} = -1 @f$ if
+/// @p signs[j] is `-0.0`. Other values for @p signs are not allowed.
 struct UpDowndate {
     std::span<const real_t> signs;
 };
+/// Perform a factorization downdate or update, depending on the given signs,
+/// i.e. given the factorization of @f$ K @f$, compute the factorization of
+/// @f$ K - A S A^\top @f$, where @f$ S @f$ is a diagonal matrix with
+/// @f$ S_{jj} = 1 @f$ if @p signs[j] is `+0.0`, and @f$ S_{jj} = -1 @f$ if
+/// @p signs[j] is `-0.0`. Other values for @p signs are not allowed.
 struct DownUpdate {
     std::span<const real_t> signs;
 };
