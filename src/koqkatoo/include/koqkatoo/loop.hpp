22cbc0fbafca08d151c270481c02daad25848312
Pieter Pas <pieter.pas@outlook.com>
2024-12-10 22:28:10 +0100

    Add foreach_chunked_merged_parallel

--- src/koqkatoo/include/koqkatoo/loop.hpp
+++ src/koqkatoo/include/koqkatoo/loop.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <koqkatoo/config.hpp>
+#include <koqkatoo/openmp.h>
 
 namespace koqkatoo {
 
@@ -47,6 +48,35 @@ foreach_chunked_merged(index_t i_begin, index_t i_end, auto chunk_size,
             func_chunk(i, rem_i);
         for (i -= chunk_size; i >= i_begin; i -= chunk_size)
             func_chunk(i, chunk_size);
+    }
+}
+
+[[gnu::always_inline]] inline void
+foreach_chunked_merged_parallel(index_t i_begin, index_t i_end, auto chunk_size,
+                                auto func_chunk,
+                                LoopDir dir = LoopDir::Forward) {
+    const index_t rem_i = (i_end - i_begin) % chunk_size;
+    if (dir == LoopDir::Forward) {
+        KOQKATOO_OMP(parallel) {
+            KOQKATOO_OMP(for nowait)
+            for (index_t i = i_begin; i <= i_end - chunk_size; i += chunk_size)
+                func_chunk(i, chunk_size);
+            KOQKATOO_OMP(single) {
+                if (rem_i > 0)
+                    func_chunk(i_end - rem_i, rem_i);
+            }
+        }
+    } else {
+        const index_t i_last = i_end - rem_i;
+        KOQKATOO_OMP(parallel) {
+            KOQKATOO_OMP(single nowait) {
+                if (rem_i > 0)
+                    func_chunk(i_last, rem_i);
+            }
+            KOQKATOO_OMP(for)
+            for (index_t i = i_last - chunk_size; i >= i_begin; i -= chunk_size)
+                func_chunk(i, chunk_size);
+        }
     }
 }
 
