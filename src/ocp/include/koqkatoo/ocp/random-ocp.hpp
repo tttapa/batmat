7a5c7694feff11b4fc55c34c287bbeeb27b058b8
Pieter Pas <pieter.pas@outlook.com>
2025-01-02 21:29:27 +0100

    Revert "Always generate positive definite random OCPs using syrk"
    This reverts commit 8360374fb2559ea35d880d8b9d096263c5494865.

--- src/ocp/include/koqkatoo/ocp/random-ocp.hpp
+++ src/ocp/include/koqkatoo/ocp/random-ocp.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <koqkatoo/linalg-compact/matrix-batch.hpp>
-#include <koqkatoo/linalg/blas-interface.hpp>
 #include <koqkatoo/ocp/ocp.hpp>
 #include <random>
 
@@ -15,32 +13,27 @@ inline LinearOCPStorage generate_random_ocp(OCPDim dim,
     std::mt19937 rng{seed};
     std::normal_distribution<real_t> nrml{0, 1};
     std::bernoulli_distribution bernoulli{0.5};
-    std::vector<real_t> work_storage((nx + nu) * (nx + nu));
-    guanaqo::MatrixView<real_t, index_t> work{{
-        .data = work_storage.data(),
-        .rows = nx + nu,
-        .cols = nx + nu,
-    }};
 
     // Dynamics and constraint matrices
     for (index_t i = 0; i < N; ++i) {
         auto Ai = ocp.A(i), Bi = ocp.B(i), Ci = ocp.C(i), Di = ocp.D(i);
-        auto Hi = ocp.H(i);
+        auto Qi = ocp.Q(i), Ri = ocp.R(i);
         Ai.generate([&] { return nrml(rng) / 2; });
         Bi.generate([&] { return nrml(rng); });
         Ci.generate([&] { return nrml(rng); });
         Di.generate([&] { return nrml(rng); });
-        work.generate([&] { return nrml(rng); });
-        linalg::xsyrk(CblasColMajor, CblasLower, CblasNoTrans, nx + nu, nx + nu,
-                      real_t{1}, work.data, work.outer_stride, real_t{0},
-                      Hi.data, Hi.outer_stride);
+        Qi.generate([&] { return nrml(rng); });
+        Ri.generate([&] { return nrml(rng); });
+        for (index_t j = 0; j < nx; ++j)
+            Qi(j, j) += 10;
+        for (index_t j = 0; j < nu; ++j)
+            Ri(j, j) += 10;
     }
     auto Ci = ocp.C(N), Qi = ocp.Q(N);
     Ci.generate([&] { return nrml(rng); });
-    work.top_left(nx, nx).generate([&] { return nrml(rng); });
-    linalg::xsyrk(CblasColMajor, CblasLower, CblasNoTrans, nx, nx, real_t{1},
-                  work.data, work.outer_stride, real_t{0}, Qi.data,
-                  Qi.outer_stride);
+    Qi.generate([&] { return nrml(rng); });
+    for (index_t j = 0; j < nx; ++j)
+        Qi(j, j) += 25;
     return ocp;
 }
 
