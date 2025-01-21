8fa2549e059287ce5919067a4f57bc548e450278
Pieter Pas <pieter.pas@outlook.com>
2025-01-21 16:54:38 +0100

    [Conan] Preserve Git commit hash

--- src/cmake/BuildTime.cmake
+++ src/cmake/BuildTime.cmake
@@ -1,9 +1,15 @@
 string(TIMESTAMP KOQKATOO_BUILD_TIME UTC)
-execute_process(
-    COMMAND git log -1 --format=%H
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-    OUTPUT_VARIABLE KOQKATOO_COMMIT_HASH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    ERROR_QUIET)
+set(COMMIT_TXT "${CMAKE_CURRENT_LIST_DIR}/../../commit.txt")
+if (EXISTS "${COMMIT_TXT}")
+    file(STRINGS ${COMMIT_TXT} KOQKATOO_COMMIT_HASH LIMIT_COUNT 1)
+    message("Read Git commit hash from file: ${KOQKATOO_COMMIT_HASH}")
+else()
+    execute_process(
+        COMMAND git log -1 --format=%H
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        OUTPUT_VARIABLE KOQKATOO_COMMIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET)
+endif()
 configure_file(${CMAKE_CURRENT_LIST_DIR}/koqkatoo-build-time.cpp.in
     koqkatoo-build-time.cpp @ONLY)
