f5b961059ad1d2c3d64968bc8c4947aa7fc3b370
Pieter Pas <pieter.pas@outlook.com>
2025-01-29 14:49:36 +0100

    Optimized SIMD rotate implementations

--- src/linalg-compact/include/koqkatoo/linalg-compact/compact/micro-kernels/rotate.hpp
+++ src/linalg-compact/include/koqkatoo/linalg-compact/compact/micro-kernels/rotate.hpp
@@ -6,64 +6,50 @@ namespace koqkatoo::linalg::compact::micro_kernels {
 
 namespace stdx = std::experimental;
 
+namespace detail {
+
 template <size_t S, class F, class Abi>
 [[gnu::always_inline]] inline stdx::simd<F, Abi> rotl(stdx::simd<F, Abi> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        stdx::simd<F, Abi> y;
-        for (size_t j = 0; j < x.size() - S; ++j)
-            y[j] = x[j + S];
-        for (size_t j = x.size() - S; j < x.size(); ++j)
-            y[j] = x[j + S - x.size()];
-        return y;
-    }
+    static_assert(S < x.size());
+    stdx::simd<F, Abi> y;
+    for (size_t j = 0; j < x.size() - S; ++j)
+        y[j] = x[j + S];
+    for (size_t j = x.size() - S; j < x.size(); ++j)
+        y[j] = x[j + S - x.size()];
+    return y;
 }
 
 template <size_t S, class F, class Abi>
 [[gnu::always_inline]] inline stdx::simd<F, Abi> rotr(stdx::simd<F, Abi> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        stdx::simd<F, Abi> y;
-        for (size_t j = x.size() - S; j < x.size(); ++j)
-            y[j + S - x.size()] = x[j];
-        for (size_t j = 0; j < x.size() - S; ++j)
-            y[j + S] = x[j];
-        return y;
-    }
+    static_assert(S < x.size());
+    stdx::simd<F, Abi> y;
+    for (size_t j = x.size() - S; j < x.size(); ++j)
+        y[j + S - x.size()] = x[j];
+    for (size_t j = 0; j < x.size() - S; ++j)
+        y[j + S] = x[j];
+    return y;
 }
 
 template <size_t S, class F, class Abi>
 [[gnu::always_inline]] inline stdx::simd<F, Abi> shiftl(stdx::simd<F, Abi> x) {
-    if constexpr (S % x.size() == 0) {
-        return stdx::simd<F, Abi>{0};
-    } else {
-        static_assert(S < x.size());
-        stdx::simd<F, Abi> y;
-        for (size_t j = 0; j < x.size() - S; ++j)
-            y[j] = x[j + S];
-        for (size_t j = x.size() - S; j < x.size(); ++j)
-            y[j] = 0;
-        return y;
-    }
+    static_assert(S < x.size());
+    stdx::simd<F, Abi> y;
+    for (size_t j = 0; j < x.size() - S; ++j)
+        y[j] = x[j + S];
+    for (size_t j = x.size() - S; j < x.size(); ++j)
+        y[j] = 0;
+    return y;
 }
 
 template <size_t S, class F, class Abi>
 [[gnu::always_inline]] inline stdx::simd<F, Abi> shiftr(stdx::simd<F, Abi> x) {
-    if constexpr (S % x.size() == 0) {
-        return stdx::simd<F, Abi>{0};
-    } else {
-        static_assert(S < x.size());
-        stdx::simd<F, Abi> y;
-        for (size_t j = x.size() - S; j < x.size(); ++j)
-            y[j + S - x.size()] = 0;
-        for (size_t j = 0; j < x.size() - S; ++j)
-            y[j + S] = x[j];
-        return y;
-    }
+    static_assert(S < x.size());
+    stdx::simd<F, Abi> y;
+    for (size_t j = x.size() - S; j < x.size(); ++j)
+        y[j + S - x.size()] = 0;
+    for (size_t j = 0; j < x.size() - S; ++j)
+        y[j + S] = x[j];
+    return y;
 }
 
 #if defined(__AVX512F__)
@@ -71,33 +57,69 @@ template <size_t S, class F, class Abi>
 template <size_t S>
 [[gnu::always_inline]] inline auto
 rotl(stdx::simd<double, stdx::simd_abi::deduce_t<double, 8>> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        constexpr size_t N = x.size();
-        const __m512i indices =
-            _mm512_set_epi64((S + 7) % N, (S + 6) % N, (S + 5) % N, (S + 4) % N,
-                             (S + 3) % N, (S + 2) % N, (S + 1) % N, S % N);
-        __m512d y = _mm512_permutexvar_pd(indices, static_cast<__m512d>(x));
-        return decltype(x){y};
-    }
+    static_assert(S < x.size());
+    constexpr size_t N = x.size();
+    const __m512i indices =
+        _mm512_set_epi64((S + 7) % N, (S + 6) % N, (S + 5) % N, (S + 4) % N,
+                         (S + 3) % N, (S + 2) % N, (S + 1) % N, S % N);
+    __m512d y = _mm512_permutexvar_pd(indices, static_cast<__m512d>(x));
+    return decltype(x){y};
 }
 
 template <size_t S>
 [[gnu::always_inline]] inline auto
 rotr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 8>> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        constexpr size_t N    = x.size();
-        const __m512i indices = _mm512_set_epi64(
-            (N - S + 7) % N, (N - S + 6) % N, (N - S + 5) % N, (N - S + 4) % N,
-            (N - S + 3) % N, (N - S + 2) % N, (N - S + 1) % N, (N - S) % N);
-        __m512d y = _mm512_permutexvar_pd(indices, static_cast<__m512d>(x));
-        return decltype(x){y};
-    }
+    static_assert(S < x.size());
+    constexpr size_t N    = x.size();
+    const __m512i indices = _mm512_set_epi64(
+        (N - S + 7) % N, (N - S + 6) % N, (N - S + 5) % N, (N - S + 4) % N,
+        (N - S + 3) % N, (N - S + 2) % N, (N - S + 1) % N, (N - S) % N);
+    __m512d y = _mm512_permutexvar_pd(indices, static_cast<__m512d>(x));
+    return decltype(x){y};
+}
+
+template <size_t S>
+[[gnu::always_inline]] inline auto
+shiftl(stdx::simd<double, stdx::simd_abi::deduce_t<double, 8>> x) {
+    static_assert(S < x.size());
+    constexpr uint8_t mask = (1u << (x.size() - S)) - 1u;
+    auto y                 = static_cast<__m512d>(rotl<S>(x));
+    y                      = _mm512_mask_blend_pd(mask, _mm512_set1_pd(0), y);
+    return decltype(x){y};
+}
+
+template <size_t S>
+[[gnu::always_inline]] inline auto
+shiftr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 8>> x) {
+    static_assert(S < x.size());
+    constexpr uint8_t mask = (1u << S) - 1u;
+    auto y                 = static_cast<__m512d>(rotr<S>(x));
+    y                      = _mm512_mask_blend_pd(mask, y, _mm512_set1_pd(0));
+    return decltype(x){y};
+}
+
+#endif
+
+#if defined(__AVX2__)
+
+template <size_t S>
+[[gnu::always_inline]] inline auto
+shiftl(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
+    static_assert(S < x.size());
+    constexpr uint8_t mask = (1u << (x.size() - S)) - 1u;
+    auto y                 = static_cast<__m256d>(rotl<S>(x));
+    y                      = _mm256_blend_pd(_mm256_set1_pd(0), y, mask);
+    return decltype(x){y};
+}
+
+template <size_t S>
+[[gnu::always_inline]] inline auto
+shiftr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
+    static_assert(S < x.size());
+    constexpr uint8_t mask = (1u << S) - 1u;
+    auto y                 = static_cast<__m256d>(rotr<S>(x));
+    y                      = _mm256_blend_pd(y, _mm256_set1_pd(0), mask);
+    return decltype(x){y};
 }
 
 #endif
@@ -107,31 +129,23 @@ rotr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 8>> x) {
 template <size_t S>
 [[gnu::always_inline]] inline auto
 rotl(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        constexpr size_t N = x.size();
-        const __m256i indices =
-            _mm256_set_epi64x((S + 3) % N, (S + 2) % N, (S + 1) % N, S % N);
-        __m256d y = _mm256_permutexvar_pd(indices, static_cast<__m256d>(x));
-        return decltype(x){y};
-    }
+    static_assert(S < x.size());
+    constexpr size_t N = x.size();
+    const __m256i indices =
+        _mm256_set_epi64x((S + 3) % N, (S + 2) % N, (S + 1) % N, S % N);
+    __m256d y = _mm256_permutexvar_pd(indices, static_cast<__m256d>(x));
+    return decltype(x){y};
 }
 
 template <size_t S>
 [[gnu::always_inline]] inline auto
 rotr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        constexpr size_t N    = x.size();
-        const __m256i indices = _mm256_set_epi64x(
-            (N - S + 3) % N, (N - S + 2) % N, (N - S + 1) % N, (N - S) % N);
-        __m256d y = _mm256_permutexvar_pd(indices, static_cast<__m256d>(x));
-        return decltype(x){y};
-    }
+    static_assert(S < x.size());
+    constexpr size_t N    = x.size();
+    const __m256i indices = _mm256_set_epi64x((N - S + 3) % N, (N - S + 2) % N,
+                                              (N - S + 1) % N, (N - S) % N);
+    __m256d y = _mm256_permutexvar_pd(indices, static_cast<__m256d>(x));
+    return decltype(x){y};
 }
 
 #elif defined(__AVX2__)
@@ -139,34 +153,64 @@ rotr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
 template <size_t S>
 [[gnu::always_inline]] inline auto
 rotl(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        constexpr size_t N    = x.size();
-        constexpr int indices = (((S + 3) % N) << 6) | (((S + 2) % N) << 4) |
-                                (((S + 1) % N) << 2) | (S % N);
-        __m256d y = _mm256_permute4x64_pd(static_cast<__m256d>(x), indices);
-        return decltype(x){y};
-    }
+    static_assert(S < x.size());
+    constexpr size_t N    = x.size();
+    constexpr int indices = (((S + 3) % N) << 6) | (((S + 2) % N) << 4) |
+                            (((S + 1) % N) << 2) | (S % N);
+    __m256d y = _mm256_permute4x64_pd(static_cast<__m256d>(x), indices);
+    return decltype(x){y};
 }
 
 template <size_t S>
 [[gnu::always_inline]] inline auto
 rotr(stdx::simd<double, stdx::simd_abi::deduce_t<double, 4>> x) {
-    if constexpr (S % x.size() == 0) {
-        return x;
-    } else {
-        static_assert(S < x.size());
-        constexpr size_t N    = x.size();
-        constexpr int indices = (((N - S + 3) % N) << 6) |
-                                (((N - S + 2) % N) << 4) |
-                                (((N - S + 1) % N) << 2) | ((N - S) % N);
-        __m256d y = _mm256_permute4x64_pd(static_cast<__m256d>(x), indices);
-        return decltype(x){y};
-    }
+    static_assert(S < x.size());
+    constexpr size_t N    = x.size();
+    constexpr int indices = (((N - S + 3) % N) << 6) |
+                            (((N - S + 2) % N) << 4) |
+                            (((N - S + 1) % N) << 2) | ((N - S) % N);
+    __m256d y = _mm256_permute4x64_pd(static_cast<__m256d>(x), indices);
+    return decltype(x){y};
 }
 
 #endif
+
+} // namespace detail
+
+template <size_t S, class F, class Abi>
+[[gnu::always_inline]] inline stdx::simd<F, Abi> rotl(stdx::simd<F, Abi> x) {
+    if constexpr (S % x.size() == 0)
+        return x;
+    else
+        return detail::rotl<S>(x);
+}
+
+template <size_t S, class F, class Abi>
+[[gnu::always_inline]] inline stdx::simd<F, Abi> rotr(stdx::simd<F, Abi> x) {
+    if constexpr (S % x.size() == 0)
+        return x;
+    else
+        return detail::rotr<S>(x);
+}
+
+template <size_t S, class F, class Abi>
+[[gnu::always_inline]] inline stdx::simd<F, Abi> shiftl(stdx::simd<F, Abi> x) {
+    if constexpr (S == 0)
+        return x;
+    else if constexpr (S >= x.size())
+        return stdx::simd<F, Abi>{0};
+    else
+        return detail::shiftl<S>(x);
+}
+
+template <size_t S, class F, class Abi>
+[[gnu::always_inline]] inline stdx::simd<F, Abi> shiftr(stdx::simd<F, Abi> x) {
+    if constexpr (S == 0)
+        return x;
+    else if constexpr (S >= x.size())
+        return stdx::simd<F, Abi>{0};
+    else
+        return detail::shiftr<S>(x);
+}
 
 } // namespace koqkatoo::linalg::compact::micro_kernels
