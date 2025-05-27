b1ad75566187dc51c6f1788cdfd39002c3ba41d3
Pieter Pas <pieter.pas@outlook.com>
2025-05-27 14:13:25 +0200

    Uniform rng

--- src/ocp/include/koqkatoo/ocp/random-ocp.hpp
+++ src/ocp/include/koqkatoo/ocp/random-ocp.hpp
@@ -11,20 +11,20 @@ inline LinearOCPStorage generate_random_ocp(OCPDim dim,
     auto [N, nx, nu, ny, ny_N] = ocp.dim;
 
     std::mt19937 rng{seed};
-    std::normal_distribution<real_t> nrml{0, 1};
+    std::uniform_real_distribution<real_t> uni{-1, 1};
     std::bernoulli_distribution bernoulli{0.5};
 
     // Dynamics and constraint matrices
     for (index_t i = 0; i < N; ++i) {
         auto Ai = ocp.A(i), Bi = ocp.B(i), Ci = ocp.C(i), Di = ocp.D(i);
         auto Qi = ocp.Q(i), Ri = ocp.R(i), Si = ocp.S(i), Hi = ocp.H(i);
-        Ai.generate([&] { return nrml(rng) * 0.9; });
-        Bi.generate([&] { return nrml(rng); });
-        Ci.generate([&] { return nrml(rng); });
-        Di.generate([&] { return nrml(rng); });
-        Qi.generate([&] { return nrml(rng); });
-        Ri.generate([&] { return nrml(rng); });
-        Si.generate([&] { return nrml(rng); });
+        Ai.generate([&] { return uni(rng) * 0.9; });
+        Bi.generate([&] { return uni(rng); });
+        Ci.generate([&] { return uni(rng); });
+        Di.generate([&] { return uni(rng); });
+        Qi.generate([&] { return uni(rng); });
+        Ri.generate([&] { return uni(rng); });
+        Si.generate([&] { return uni(rng); });
         for (index_t j = 0; j < nx; ++j)
             Qi(j, j) += 2 * static_cast<real_t>(nx + nu);
         for (index_t j = 0; j < nu; ++j)
@@ -34,8 +34,8 @@ inline LinearOCPStorage generate_random_ocp(OCPDim dim,
                 Hi(c, r) = Hi(r, c);
     }
     auto Ci = ocp.C(N), Qi = ocp.Q(N);
-    Ci.generate([&] { return nrml(rng); });
-    Qi.generate([&] { return nrml(rng); });
+    Ci.generate([&] { return uni(rng); });
+    Qi.generate([&] { return uni(rng); });
     for (index_t j = 0; j < nx; ++j)
         Qi(j, j) += 2 * static_cast<real_t>(nx);
     for (index_t c = 0; c < nx; ++c)
