05524a97da6d751692a6cc76aff3ff1027afaafd
Pieter Pas <pieter.p.dev@outlook.com>
2025-06-25 17:27:46 +0200

    Use generator constructor for SIMD masks

--- src/batmat/include/batmat/linalg/uview.hpp
+++ src/batmat/include/batmat/linalg/uview.hpp
@@ -37,14 +37,22 @@ struct simd_view_types {
         if constexpr (MaskL == 0) {
             datapar::aligned_store(x, p);
         } else if constexpr (MaskL > 0) {
+#if BATMAT_WITH_GSI_HPC_SIMD
+            typename simd::mask_type m{[&](size_t i) { return i >= MaskL; }};
+#else
             typename simd::mask_type m{};
             for (int i = 0; i < m.size(); ++i)
                 m[i] = i >= MaskL;
+#endif
             datapar::masked_aligned_store(x, m, p);
         } else {
+#if BATMAT_WITH_GSI_HPC_SIMD
+            typename simd::mask_type m{[&](size_t i) { return i < m.size() - MaskL; }};
+#else
             typename simd::mask_type m{};
             for (int i = 0; i < m.size(); ++i)
                 m[i] = i < m.size() - MaskL;
+#endif
             datapar::masked_aligned_store(x, m, p);
         }
     }
