9f2d488e645728ec8a5e3ecdbbe69990ea685e55
Pieter Pas <pieter.p.dev@outlook.com>
2025-10-25 18:21:01 +0200

    Upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -60,7 +60,7 @@ class BatmatRecipe(ConanFile):
     generators = ("CMakeDeps",)
 
     def requirements(self):
-        self.requires("guanaqo/1.0.0-alpha.18", transitive_headers=True, transitive_libs=True)
+        self.requires("guanaqo/1.0.0-alpha.20", transitive_headers=True, transitive_libs=True)
         if self.options.get_safe("with_benchmarks"):
             self.requires("benchmark/1.9.4")
             self.requires("hyhound/1.0.2-alpha.2")
