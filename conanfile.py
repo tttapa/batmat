75a7c0c0a5fb617b00575e86afda33aa6743bf0a
Pieter Pas <pieter.p.dev@outlook.com>
2025-11-18 16:01:51 +0100

    Upgrade hyhound

--- conanfile.py
+++ conanfile.py
@@ -63,7 +63,7 @@ class BatmatRecipe(ConanFile):
         self.requires("guanaqo/1.0.0-alpha.21", transitive_headers=True, transitive_libs=True)
         if self.options.get_safe("with_benchmarks"):
             self.requires("benchmark/1.9.4")
-            self.requires("hyhound/1.0.2-alpha.2")
+            self.requires("hyhound/1.1.0")
         if self.options.get_safe("with_openmp") and self.settings.compiler == "clang":
             self.requires(f"llvm-openmp/[~{self.settings.compiler.version}]")
         if self.options.get_safe("with_gsi_hpc_simd"):
