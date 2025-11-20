6dac61268e78430a52ea43c2b982e67737e76ac1
Pieter Pas <pieter.p.dev@outlook.com>
2025-11-20 23:59:23 +0100

    Version 0.0.8

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ from conan.tools.scm import Git
 
 class BatmatRecipe(ConanFile):
     name = "batmat"
-    version = "0.0.7"
+    version = "0.0.8"
 
     license = "LGPL-3.0-or-later"
     author = "Pieter P <pieter.p.dev@outlook.com>"
