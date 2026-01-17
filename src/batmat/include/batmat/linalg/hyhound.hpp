78c51f21eacac988b4fe257ce9ac073e2fe60a6e
Pieter Pas <pieter.p.dev@outlook.com>
2026-01-17 15:03:15 +0100

    Enable row-major inputs for hyhound

--- src/batmat/include/batmat/linalg/hyhound.hpp
+++ src/batmat/include/batmat/linalg/hyhound.hpp
@@ -203,8 +203,7 @@ template <MatrixStructure SL, simdifiable VL1, simdifiable VA1, simdifiable VL2,
           simdifiable Vd>
     requires simdify_compatible<VL1, VA1, VL2, VA2, Vd>
 void hyhound_diag_2(Structured<VL1, SL> L1, VA1 &&A1, VL2 &&L2, VA2 &&A2, Vd &&d) {
-    detail::hyhound_diag_2<simdified_value_t<VL1>, simdified_abi_t<VL1>, {}, StorageOrder::ColMajor,
-                           StorageOrder::ColMajor, StorageOrder::ColMajor, StorageOrder::ColMajor>(
+    detail::hyhound_diag_2<simdified_value_t<VL1>, simdified_abi_t<VL1>, {}>(
         simdify(L1.value), simdify(A1), simdify(L2), simdify(A2), simdify(d).as_const());
 }
 
@@ -221,9 +220,7 @@ template <MatrixStructure SL, simdifiable VL11, simdifiable VA1, simdifiable VL2
     requires simdify_compatible<VL11, VA1, VL21, VA2, VA2o, VU, VA3, VA3o, Vd>
 void hyhound_diag_cyclic(Structured<VL11, SL> L11, VA1 &&A1, VL21 &&L21, VA2 &&A2, VA2o &&A2_out,
                          VU &&L31, VA3 &&A3, VA3o &&A3_out, Vd &&d, index_t split_A) {
-    detail::hyhound_diag_cyclic<simdified_value_t<VL11>, simdified_abi_t<VL11>, {},
-                                StorageOrder::ColMajor, StorageOrder::ColMajor,
-                                StorageOrder::ColMajor, StorageOrder::ColMajor>(
+    detail::hyhound_diag_cyclic<simdified_value_t<VL11>, simdified_abi_t<VL11>, {}>(
         simdify(L11.value), simdify(A1), simdify(L21), simdify(A2).as_const(), simdify(A2_out),
         simdify(L31), simdify(A3).as_const(), simdify(A3_out), simdify(d).as_const(), split_A);
 }
@@ -241,9 +238,7 @@ template <MatrixStructure SL, simdifiable VL11, simdifiable VA1, simdifiable VL2
     requires simdify_compatible<VL11, VA1, VL21, VA2, VA2o, VLu1, VAuo, Vd>
 void hyhound_diag_riccati(Structured<VL11, SL> L11, VA1 &&A1, VL21 &&L21, VA2 &&A2, VA2o &&A2_out,
                           VLu1 &&Lu1, VAuo &&Au_out, Vd &&d, bool shift_A_out = false) {
-    detail::hyhound_diag_riccati<simdified_value_t<VL11>, simdified_abi_t<VL11>, {},
-                                 StorageOrder::ColMajor, StorageOrder::ColMajor,
-                                 StorageOrder::ColMajor, StorageOrder::ColMajor>(
+    detail::hyhound_diag_riccati<simdified_value_t<VL11>, simdified_abi_t<VL11>, {}>(
         simdify(L11.value), simdify(A1), simdify(L21), simdify(A2).as_const(), simdify(A2_out),
         simdify(Lu1), simdify(Au_out), simdify(d).as_const(), shift_A_out);
 }
